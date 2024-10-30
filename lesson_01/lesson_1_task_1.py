8d752880d982f7970266837db75d127f9fa7f122
Fidan <fidanbootman@gmail.com>
2024-10-31 00:40:11 +0400

    lesson_01

---
+++ lesson_01/lesson_1_task_1.py
@@ -0,0 +1,2 @@
+my_name = "Fidan"
+print(my_name)
---
+++ lesson_01/lesson_1_task_3.py
@@ -0,0 +1,4 @@
+first_name = input("Введите ваше имя: Fidan ")
+last_name = input("Введите вашу фамилию: Mahmudova ")
+
+print( "Вас зовут " + first_name + last_name )
